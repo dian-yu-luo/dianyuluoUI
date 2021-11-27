aadb580df28146e42ad751374deb84b548eaa1cd
dianyuluo <1739556970@qq.com>
2021-11-27 21:48:30 +0800

    learn source code

--- src/init.cpp
+++ src/init.cpp
@@ -7,6 +7,7 @@ static void glfw_error_callback(int error, const char *description)
 
 GLFWwindow *init()
 {
+    // glfw的基础选项配置
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit())
         exit(1);
@@ -15,21 +16,30 @@ GLFWwindow *init()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-    GLFWwindow *window = glfwCreateWindow(2160, 1080, "dianyuluo", NULL, NULL);
+    // 可以添加全屏模式，添加到配置文件里
+
+    // bool isFullScreen = true;
+    // int monitorCount;
+    GLFWmonitor *pMonitor = glfwGetPrimaryMonitor();
+    pMonitor = nullptr;
+
+    GLFWwindow *window = glfwCreateWindow(2160, 1080, "dianyuluo", pMonitor, NULL);
 
     if (window == NULL)
         exit(1);
+
+    //who can tell me what's meaning it is
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
     IMGUI_CHECKVERSION();
+    // there ! nothing has come back,who has be influeced
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
     (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 
     ImGui::StyleColorsDark();
-  
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
